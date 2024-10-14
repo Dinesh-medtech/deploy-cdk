2e50a798bfb63a19574198b66435ce5af2d9e133
Dinesh-medtech <dineshkumar@medtechsoft.co>
2024-10-14 12:59:26 +0530

    code update

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,78 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
-import { CfnOutput } from "aws-cdk-lib";
-import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from 'constructs';
-import { Subnet } from 'aws-cdk-lib/aws-ec2';
-import { createS3GatewayEndpoint, createVpc } from './resources/vpc';
-import { createKeyMultiRegion } from './resources/kms';
-import { createALB, createApplicationLoadBalancer } from './resources/alb';
+import { VpcStack } from './resources/vpc';
+import { LoadBalancerStack } from './resources/alb';
+import { KmsStack } from './resources/kms';
 
-export class NetworkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class MainStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const multiRegionResources = ["rds"];
+    // VPC and S3 Endpoint
+    const vpcStack = new VpcStack(this, 'VpcStack');
 
-    /**
-     * Function for creating vpc
-     * @param scope - cdk scope
-     * @param natGateways - No of natGateways
-     * @param maxAzs - maxAzs
-     */
-    const vpc = createVpc(this, 0, 2)
-    new CfnOutput(
-      this,
-      `vpc-export`,
-      {
-        value: vpc.vpcId,
-        exportName: "my-app-vpcId",
-      }
-    );
+    // Load Balancer 
+    new LoadBalancerStack(this, 'LoadBalancerStack', {
+      vpc: vpcStack.vpc,
+      securityGroup: vpcStack.securityGroup,
+    });
 
-    new CfnOutput(
-      this,
-      `subnet-export`,
-      {
-        value: vpc.publicSubnets.map(subnet => subnet.subnetId).join(','),
-        exportName: "my-app-Subnets",
-      }
-    );
-
-    new CfnOutput(
-      this,
-      `public-subnet1-export`,
-      {
-        value: vpc.publicSubnets[0].subnetId,
-        exportName: "my-app-Public-Subnet1",
-      }
-    );
-
-    new CfnOutput(
-      this,
-      `public-subnet2-export`,
-      {
-        value: vpc.publicSubnets[1].subnetId,
-        exportName: "my-app-Public-Subnet2",
-      }
-    );
-
-    const alb = createApplicationLoadBalancer(this, vpc.vpcId, vpc.publicSubnets, vpc.vpcCidrBlock)
-
-    if(process.env.ENV === "development"){
-      const testALB = createALB(this, vpc.vpcId, vpc.publicSubnets, vpc.vpcCidrBlock, "public-partner-my-app-alb-test", "Public-Alb-test-SecurityGroup", "Public-Partner-my-app-Alb-Arn-test", "Public-Partner-my-app-Alb-test-sg")
-
-    }
-    // Create S3 Gateway Endpoint
-    const createS3Gatewayendpoint = createS3GatewayEndpoint(this, vpc)
-
-    //Create Multi-Regional Keys
-    if (process.env.ENV) {
-      createKeyMultiRegion(
-        this,
-        process.env.ENV,
-        "my-app",
-        "my-app"
-      );
-    }
+    // KMS Key
+    new KmsStack(this, 'KmsStack');
   }
 }
--- cdk/lib/resources/kms.ts
+++ cdk/lib/resources/kms.ts
@@ -1,58 +1,16 @@
-import { CfnOutput, RemovalPolicy, Stack, aws_iam } from "aws-cdk-lib";
-import * as kms from "aws-cdk-lib/aws-kms";
-import { Construct } from "constructs";
+import * as cdk from 'aws-cdk-lib';
+import { Key } from 'aws-cdk-lib/aws-kms';
+import { Construct } from 'constructs';
 
-export function createKeyMultiRegion(
-    scope: Construct,
-    environment: string,
-    name: string,
-    alias?: string
-  ) {
-  
-    const key = new kms.Key(scope, `${name}-mr-${environment}`, {
-      removalPolicy: RemovalPolicy.RETAIN,
-      enabled: true,
+export class KmsStack extends cdk.Stack {
+  public readonly kmsKey: Key;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // KMS Key
+    this.kmsKey = new Key(this, 'MyKmsKey', {
       enableKeyRotation: true,
-      description: `${name} Multi Region key`,
-      // Initially, we set an empty policy, we will update it in the next step
-      policy: new aws_iam.PolicyDocument(),
     });
-  
-    // Allow IAM policies
-    key.addToResourcePolicy(new aws_iam.PolicyStatement({
-      actions: [
-        'kms:*'
-      ],
-      principals: [new aws_iam.ArnPrincipal(`arn:aws:iam::${Stack.of(scope).account}:root`)],
-      resources: ['*'],
-    }));
-
-    const cfnKey = key.node.defaultChild as kms.CfnKey;
-    cfnKey.addPropertyOverride("MultiRegion", true);
-  
-    if (alias) {
-      const a = new kms.Alias(scope, `${name}-alias-mr-${environment}`, {
-        aliasName: alias,
-        targetKey: key,
-      });
-  
-      new CfnOutput(
-        scope,
-        `${name.toLowerCase()}-${environment}MrKmsKeyAlaisExport`,
-        {
-          value: a.aliasName,
-          exportName: `${name}-key-alias`,
-        }
-      );
-    }
-    new CfnOutput(
-      scope,
-      `${name.toLowerCase()}-${environment}MrKmsKeyExport`,
-      {
-        value: key.keyArn,
-        exportName: `${name}-key`,
-      }
-    );
-    return key.keyArn;
+  }
 }
-  

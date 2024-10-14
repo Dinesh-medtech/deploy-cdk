244604c0986129130d073d86a11a5bf70022ae1c
Dinesh-medtech <dineshkumar@medtechsoft.co>
2024-10-14 11:59:48 +0530

    Update vpc.ts

--- cdk/lib/resources/vpc.ts
+++ cdk/lib/resources/vpc.ts
@@ -1,73 +1,39 @@
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as cdk from 'aws-cdk-lib';
-import { Stack, Tags } from "aws-cdk-lib";
-import { Construct } from "constructs";
-import {
-  CfnVPCEndpoint,
-  VpcEndpointType,
-} from "aws-cdk-lib/aws-ec2";
+import { Vpc, SubnetType, GatewayVpcEndpointAwsService, SecurityGroup } from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
 
-export function createVpc(
-    scope: Construct,
-    natGateways: number,
-    az: number
-  ) {
-    const vpc = new ec2.Vpc(scope, 'my-app-VPC', {
-        ipAddresses: ec2.IpAddresses.cidr("10.1.0.0/16"),
-        natGateways: natGateways,
-        maxAzs: az,
-        subnetConfiguration: [
-          {
-            name: 'genie-public-subnet-1',
-            subnetType: ec2.SubnetType.PUBLIC,
-            cidrMask: 24,
-          },
-          {
-            name: 'genie-public-subnet-2',
-            subnetType: ec2.SubnetType.PUBLIC,
-            cidrMask: 24,
-          },
-          {
-            name: 'genie-private-subnet-1',
-            subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-            cidrMask: 24,
-          },
-          {
-            name: 'genie-private-subnet-2',
-            subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-            cidrMask: 24,
-          },
-        ],
-      });
+export class VpcStack extends cdk.Stack {
+  public readonly vpc: Vpc;
+  public readonly securityGroup: SecurityGroup;
 
-    return vpc;
-}
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-export function createS3GatewayEndpoint(
-  scope: Construct,
-  Vpc: ec2.Vpc
-) {
+    // VPC with subnets
+    this.vpc = new Vpc(this, 'MyVpc', {
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: 'PublicSubnet',
+          subnetType: SubnetType.PUBLIC,  
+        },
+        {
+          name: 'PrivateSubnet',
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS, 
+        }
+      ]
+    });
 
-  // Define an array to hold the route table IDs
-  let routeTableIds: string[] = [];
+    // Security Group for resources within the VPC
+    this.securityGroup = new SecurityGroup(this, 'MyVpcSecurityGroup', {
+      vpc: this.vpc,
+      allowAllOutbound: true,
+    });
 
-  const privateSubnets = Vpc.privateSubnets
-  privateSubnets.forEach((sub, index) => {
-    routeTableIds.push(sub.routeTable.routeTableId);
-  });
-
-  const publicSubnets = Vpc.publicSubnets
-  publicSubnets.forEach((sub, index) => {
-    routeTableIds.push(sub.routeTable.routeTableId);
-  });
-
-  // Create a VPC Gateway Endpoint for s3
-  const s3GatewayEndpoint = new CfnVPCEndpoint(scope, `S3GatewayEndpoint`, {
-    vpcId: Vpc.vpcId,
-    serviceName: ec2.GatewayVpcEndpointAwsService.S3.name,
-    vpcEndpointType: VpcEndpointType.GATEWAY,
-    routeTableIds: routeTableIds
-  });
-
-  return s3GatewayEndpoint;
+    // S3 Gateway Endpoint
+    this.vpc.addGatewayEndpoint('S3Endpoint', {
+      service: GatewayVpcEndpointAwsService.S3,
+      subnets: [{ subnetType: SubnetType.PRIVATE_WITH_EGRESS }],
+    });
+  }
 }
